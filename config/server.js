32f26c794092e9c7051f5d54d4ad04ac5aaf2db2
blewis11 <bejallewis@hotmail.com>
2021-01-09 13:57:37 +0000

    change to own droplet

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://46.101.9.88/api",
+  url: "http://138.68.65.237/api",
   admin: {
-    url: "http://46.101.9.88/dashboard",
+    url: "http://138.68.65.237/dashboard",
     auth: {
       secret: env("ADMIN_JWT_SECRET", "a8428198c9725e1ab4c290dd8da1fdde")
     }
